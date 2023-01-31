1896373d3b058b838c2072bfd6ff901d0514face
alfina5197 <alfinamansuri5197@gmail.com>
2023-01-31 18:29:10 +0530

    right-section

--- src/app/shared/material-module/material-module.module.ts
+++ src/app/shared/material-module/material-module.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatCheckboxModule
   ]
 })
 export class MaterialModuleModule { }
